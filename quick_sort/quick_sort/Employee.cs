370d29a3c62044430665c534b086444083b33f22
tuahivaa <46515923+tuahivaa@users.noreply.github.com>
2019-02-13 10:14:43 -1000

    Add files via upload

---
+++ quick_sort/quick_sort/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace quick_sort
+{
+    public class Employee
+    {
+        public string name, age, job, year;
+        public int id;
+        //int age;
+        //string job;
+        //int year;
+
+
+        /*
+         * create a constructor that separate informations of one line and assign it the our fields           
+         */
+        public Employee(string data)
+        {
+            string[] allIformation = data.Split('|');
+            name = allIformation[0];
+            id = int.Parse(allIformation[1]);
+            age = allIformation[2];
+            job = allIformation[3];
+            year = allIformation[4];
+
+        }
+
+        //the format method create and display the content in a "pretty" format
+        //public void format()
+        //{
+        //    Console.WriteLine("Name:\t\t" + name);
+        //    Console.WriteLine("ID:\t\t" + id);
+        //    Console.WriteLine("Age:\t\t" + age);
+        //    Console.WriteLine("Job:\t\t" + job);
+        //    Console.WriteLine("Year:\t\t" + year);
+        //    Console.WriteLine("-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-");
+        //}
+
+        public override string ToString()
+        {
+
+            return name + " " + id + " " + age + " " + job + " " + year;
+        }
+
+    }
+}
---
+++ quick_sort/quick_sort/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+
+namespace quick_sort
+{
+    class MainClass
+    {
+        public static int comparison, ex, recursiveCalls = 0;
+        //public static int exchange = 0;
+        //public static int recursiveCalls = 0;
+
+
+        public static void Main(string[] args)
+        {
+
+            Employee[] dataEmployee;
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("pass in parameter");
+                }
+                else
+                {
+
+                    //direclty pass the first/second command line parameter to the streamreader
+                    //args[0] = employee file
+                    StreamReader employeeFile = new StreamReader(args[0]);
+
+                    //read the very first line of the our employe file, it tells us how many employe we have in our file
+                    string firstLineSR = employeeFile.ReadLine();
+                    int numbersOfEmployee = int.Parse(firstLineSR);
+
+                    //have an array of employee ready to be used in our for loop.
+                    dataEmployee = new Employee[numbersOfEmployee];
+
+                    //for loop to go through our employee file, and create an array of employe with their informations in it
+                    for (int i = 0; i < numbersOfEmployee; i++)
+                    {
+                        string line = employeeFile.ReadLine();
+                        dataEmployee[i] = new Employee(line);
+                    }
+
+
+                    Console.WriteLine("***QUICK SORT***");
+                    MainClass aa = new MainClass();
+                    aa.write(dataEmployee);
+                    //aa.quickSort(dataEmployee, 0, dataEmployee.Length-1);
+
+                    //aa.randomizedQuicksort(dataEmployee, 0, dataEmployee.Length-1);
+
+                    aa.tailRecursiveQuickSort(dataEmployee, 0, dataEmployee.Length - 1);
+
+                    aa.write(dataEmployee);
+                    Console.WriteLine("Comparisons: " + comparison);
+                    Console.WriteLine("Exchanges: " + ex);
+                    Console.WriteLine("Recursive calls: " + (recursiveCalls)*2);
+
+
+
+
+                    StreamWriter sw = new StreamWriter("AFTER_QUICK_SORT.txt");
+                    for (int i = 0; i < dataEmployee.Length; i++)
+                    {
+                        sw.WriteLine(dataEmployee[i]);
+                    }
+                    sw.Close();
+
+
+
+                    employeeFile.Close();
+                }
+            }
+
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found.");
+            }
+
+
+        }
+
+
+        //encapsulate the logic to print the data
+        public void write( Employee[] arr )
+        {
+            for (int m = 0; m < arr.Length; m++)
+            {
+                Console.Write(" " + arr[m].id);
+            }
+            Console.WriteLine("");
+        }
+
+        private void exchange(Employee[] arr, int x, int y)
+        {
+            Employee temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+            ex++;
+            //write(arr);
+        }
+
+
+        public void quickSort( Employee[] arr, int p, int r )
+        {
+            int q = 0;
+            if ( p < r )
+            {
+
+                q = partition(arr, p, r);
+                quickSort(arr, p, q-1);
+                quickSort(arr, q+1, r);
+
+            }
+
+        }
+
+        public int partition( Employee[] arr, int p, int r )
+        {
+
+            Employee x = arr[r];
+            int i = p;
+            for ( int j = p ; j < r ; j++ )
+            {
+                comparison++;
+                if ( arr[j].id <= x.id )
+                {
+
+                    exchange(arr, i, j);
+                    i++;
+
+
+                }
+
+            }
+            //write(arr);
+            exchange(arr, i, r);
+            return i;
+
+        }
+
+        public int randomizedPartition(Employee[] arr, int p, int r)
+        {
+
+            Random i = new Random();
+            int x = i.Next(p, r);
+            exchange(arr, r, x);
+            return partition(arr, p, r);
+
+        }
+
+        public void randomizedQuicksort(Employee[] arr, int p, int r)
+        {
+            int q = 0;
+            if (p < r)
+            {
+
+                q = randomizedPartition(arr, p, r);
+                randomizedPartition(arr, p, q-1);
+                randomizedPartition(arr, q+1, r);
+            }
+
+        }
+
+        public void tailRecursiveQuickSort(Employee[] arr, int p, int r)
+        {
+            int q;
+            while ( p<r )
+            {
+
+                q = partition(arr, p, r);
+                tailRecursiveQuickSort(arr, p, q-1);
+                p = q + 1;
+                recursiveCalls++;
+
+            }
+
+        }
+
+    }
+}
